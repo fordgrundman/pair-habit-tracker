a8650f944ec055774949348b6829ab46cb06efd5
Ford <codingasford@gmail.com>
2026-03-11 10:36:23 -0700

    edited headers to attempt signup fail fix for auth microservice call

--- pair-habit-tracker/frontend-react/src/pages/AccountPage.tsx
+++ pair-habit-tracker/frontend-react/src/pages/AccountPage.tsx
@@ -10,7 +10,7 @@ const {
 
 // ecord<string, string> needed to satisfy Vite fetch HeadersInit type for typescript
 const HEADERS: Record<string, string> = {
-  "Content-Type": "application/json",
+  // "Content-Type": "application/json",
   "X-App-Id": APP_ID,
   "X-App-Secret": APP_SECRET,
 };
