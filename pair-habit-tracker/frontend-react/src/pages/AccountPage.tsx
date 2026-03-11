ec619bca4ad0c38e876782d5cdfda59c58e7dab8
Ford <codingasford@gmail.com>
2026-03-11 10:31:46 -0700

    converted login system to use an auth microservice

--- pair-habit-tracker/frontend-react/src/pages/AccountPage.tsx
+++ pair-habit-tracker/frontend-react/src/pages/AccountPage.tsx
@@ -1,5 +1,20 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+
+//Auth Microservice configurations
+const {
+  VITE_AUTH_URL: BASE_URL,
+  VITE_APP_ID: APP_ID,
+  VITE_APP_SECRET: APP_SECRET,
+} = import.meta.env;
+
+// ecord<string, string> needed to satisfy Vite fetch HeadersInit type for typescript
+const HEADERS: Record<string, string> = {
+  "Content-Type": "application/json",
+  "X-App-Id": APP_ID,
+  "X-App-Secret": APP_SECRET,
+};
+
 function AccountPage() {
   const [cachedUsername, setCachedUsername] = useState(
     () => localStorage.getItem("username") ?? "",
@@ -10,52 +25,43 @@ function AccountPage() {
   const navigate = useNavigate();
 
   const signUp = async () => {
-    const newAccount = { username, password };
-
-    const response = await fetch(
-      "https://pair-habit-tracker.onrender.com/signup",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        credentials: "include",
-        body: JSON.stringify(newAccount),
-      },
-    );
+    const signupPayload = { username: username, password: password };
+    const response = await fetch(`${BASE_URL}/signup`, {
+      method: "POST",
+      headers: HEADERS,
+      body: JSON.stringify(signupPayload),
+    });
 
     if (response.status === 201) {
+      const data = await response.json();
       localStorage.setItem("username", username);
+      localStorage.setItem("sessionId", data.sessionId);
       setCachedUsername(username);
 
-      //only navigate to Habits List after successful signup
+      //navigate to Habits List after successful signup
       navigate("/habits");
+    } else if (response.status === 409) {
+      alert("Username already exists. Please login instead.");
     } else {
-      alert("Failed to create account, username may already exist");
+      alert("Failed to create account.");
     }
   };
 
   const login = async () => {
-    const response = await fetch(
-      "https://pair-habit-tracker.onrender.com/login",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        credentials: "include",
-        body: JSON.stringify({
-          username: username,
-          password: password,
-        }),
-      },
-    );
+    const loginPayload = { username: username, password: password };
+    const response = await fetch(`${BASE_URL}/login`, {
+      method: "POST",
+      headers: HEADERS,
+      body: JSON.stringify(loginPayload),
+    });
 
     if (response.status === 200) {
+      const data = await response.json();
       localStorage.setItem("username", username);
+      localStorage.setItem("sessionId", data.sessionId);
       setCachedUsername(username);
 
-      //only navigate to Habits List after successful login
+      //navigate to Habits List after successful login
       navigate("/habits");
     } else {
       alert("Login failed. Incorrect username or password");
